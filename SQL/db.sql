326810165bb2560045296fba0fc231b01c7f38ed
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-09-02 12:54:27 -0300

    Colocado as imagens dos diagramas.

---
+++ SQL/db.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE petshop_db
+USE petshop_db;
+
+CREATE TABLE atendente(
+
+
+);
+
+CREATE TABLE pet(
+
+
+);
+
+CREATE TABLE agendamento(
+
+
+);
+
+CREATE TABLE servico(
+
+    
+);
